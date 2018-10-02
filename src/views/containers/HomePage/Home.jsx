6b8d8a4320ec341f29013d560efc8636234c5aea
nhduc0611 <nguyenhoangduc061195@gmail.com>
2018-10-02 17:34:56 +0700

    [Home] add animation

--- src/views/containers/HomePage/Home.jsx
+++ src/views/containers/HomePage/Home.jsx
@@ -19,6 +19,7 @@ import FeatureCard from '../../components/cards/FeatureCard/FeatureCard.jsx';
 import Carousel from '../../widgets/Carousel/Carousel.jsx';
 import { Image } from '../../components/Image/Image.jsx';
 import { PageWrapper } from '../../widgets/PageWrapper/PageWrapper.jsx';
+import InviewMonitor from '../../widgets/InviewMonitor/InviewMonitor.jsx';
 
 require("style-loader!./Home.scss");
 
@@ -36,6 +37,8 @@ class HomePage extends React.Component {
     this.state = {
       email: '',
     }
+
+    this.validateEmail = this.validateEmail.bind(this);
   }
 
   componentDidMount() {
@@ -55,6 +58,10 @@ class HomePage extends React.Component {
     ])
   }
 
+  validateEmail() {
+
+  }
+
   render() {
     let {activeCube} = this.state;
     const { _t, pathName } = this.props;
@@ -70,6 +77,7 @@ class HomePage extends React.Component {
       <div className={'feature-indicator'} key={'btn-3'} >{_t('market')}</div>,
       <div className={'feature-indicator'} key={'btn-4'} >{_t('battle')}</div>,
     ]
+
     return (
       <PageWrapper>
         <Navbar pathName={pathName} transforming navbarType={'home'} scrollingElement={'home-page'}/>
@@ -81,21 +89,25 @@ class HomePage extends React.Component {
           </div>
           {/* end home__banner */}
 
-
-          <Container size={Container.sizes.SMALL} className="home__intro" id={'intro'}>
-            <div className="home__intro-board">
-              <p>{'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.'}</p>
-            </div>
-            <div className="home__intro-cubego">
-              {[
-                require('../../../shared/img/assets/model_example_1.png'), 
-                require('../../../shared/img/assets/model_example_3.png'),
-                require('../../../shared/img/assets/model_example_2.png')].map((cube, idx) => (
-                  <img key={idx} src={cube}/>
-                ))
-              }              
-            </div>
-          </Container>
+          <InviewMonitor
+            classNameNotInView='vis-hidden'
+            classNameInView='animated fadeInUp'
+          >
+            <Container size={Container.sizes.SMALL} className="home__intro" id={'intro'}>
+              <div className="home__intro-board">
+                <p>{'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.'}</p>
+              </div>
+              <div className="home__intro-cubego">
+                {[
+                  require('../../../shared/img/assets/model_example_1.png'), 
+                  require('../../../shared/img/assets/model_example_3.png'),
+                  require('../../../shared/img/assets/model_example_2.png')].map((cube, idx) => (
+                    <img key={idx} src={cube}/>
+                  ))
+                }              
+              </div>
+            </Container>
+          </InviewMonitor>
           {/* end home__intro */}
 
           <Container className="home__modes" id={'modes'}>
@@ -111,53 +123,90 @@ class HomePage extends React.Component {
 
           <div className="home__game-detail" id={'game-detail'}>
 
-            <Container className={'home__game-detail__1 section'}>
-              <div className={'background yellow left'}/>
+              <Container className={'home__game-detail__1 section'}>
+                <div className={'background yellow left'}/>
 
-              <div className={'content left'}>
-                <div className={'desc'}>
-                  <Text className={'header'} type={Text.types.H1} children={_t('game_detail_index')} />
-                  <p className={'text'}>{_t('game_detail_index_desc')}</p>
-                  <ButtonNew color={ButtonNew.colors.BLACK_NO_SHADOW} label={_t('read_more')}
-                             className={'btn-btn'} onClick={() => {}}/>
-                </div>
-                <div className={'image'}>
-                  <img src={require('../../../shared/img/banner/copywrite_banner.png')}/>
-                </div>
-              </div>
-            </Container>
+                <div className={'content left'}>
 
-            <Container className={'home__game-detail__2 section'}>
-              <div className={'background blue right'}/>
+                  <InviewMonitor
+                    classNameNotInView='vis-hidden'
+                    classNameInView='animated fadeInLeft'
+                  >
+                    <div className={'desc'} >
+                      <Text className={'header'} type={Text.types.H1} children={_t('game_detail_index')} />
+                      <p className={'text'}>{_t('game_detail_index_desc')}</p>
+                      <ButtonNew color={ButtonNew.colors.BLACK_NO_SHADOW} label={_t('read_more')}
+                                className={'btn-btn'} onClick={() => {}}/>
+                    </div>
+                  </InviewMonitor>
 
-              <div className={'content right'}>
-                <div className={'desc'}>
-                  <Text className={'header'} type={Text.types.H1} children={_t('game_detail_index')} />
-                  <p className={'text'}>{_t('game_detail_index_desc')}</p>
-                  <ButtonNew color={ButtonNew.colors.BLACK_NO_SHADOW} label={_t('read_more')}
-                             className={'btn-btn'} onClick={() => {}}/>
+                  <InviewMonitor
+                    classNameNotInView='vis-hidden'
+                    classNameInView='animated zoomIn'
+                  >
+                    <div className={'image'} >
+                      <img src={require('../../../shared/img/banner/copywrite_banner.png')}/>
+                    </div>
+                  </InviewMonitor>
                 </div>
-                <div className={'image'}>
-                  <img src={require('../../../shared/img/banner/index_banner.png')}/>
-                </div>
-              </div>
-            </Container>
+              </Container>
 
-            <Container className={'home__game-detail__3 section'}>
-              <div className={'background red left'}/>
 
-              <div className={'content left'}>
-                <div className={'desc'}>
-                  <Text className={'header'} type={Text.types.H1} children={_t('game_detail_index')} />
-                  <p className={'text'}>{_t('game_detail_index_desc')}</p>
-                  <ButtonNew color={ButtonNew.colors.BLACK_NO_SHADOW} label={_t('read_more')}
-                             className={'btn-btn'} onClick={() => {}}/>
+            
+              <Container className={'home__game-detail__2 section'}>
+                <div className={'background blue right'}/>
+
+                <div className={'content right'}>
+                <InviewMonitor
+                  classNameNotInView='vis-hidden'
+                  classNameInView='animated fadeInRight'
+                >
+                  <div className={'desc'}>
+                    <Text className={'header'} type={Text.types.H1} children={_t('game_detail_index')} />
+                    <p className={'text'}>{_t('game_detail_index_desc')}</p>
+                    <ButtonNew color={ButtonNew.colors.BLACK_NO_SHADOW} label={_t('read_more')}
+                              className={'btn-btn'} onClick={() => {}}/>
+                  </div>
+                </InviewMonitor>
+                <InviewMonitor
+                  classNameNotInView='vis-hidden'
+                  classNameInView='animated zoomIn'
+                >
+                  <div className={'image'}>
+                    <img src={require('../../../shared/img/banner/index_banner.png')}/>
+                  </div>
+                </InviewMonitor>
                 </div>
-                <div className={'image'}>
-                  <img src={require('../../../shared/img/banner/index_banner.png')}/>
+              </Container>
+
+
+            
+              <Container className={'home__game-detail__3 section'}>
+                <div className={'background red left'}/>
+
+                <div className={'content left'}>
+                  <InviewMonitor
+                    classNameNotInView='vis-hidden'
+                    classNameInView='animated fadeInUp'
+                  >
+                    <div className={'desc'}>
+                      <Text className={'header'} type={Text.types.H1} children={_t('game_detail_index')} />
+                      <p className={'text'}>{_t('game_detail_index_desc')}</p>
+                      <ButtonNew color={ButtonNew.colors.BLACK_NO_SHADOW} label={_t('read_more')}
+                                className={'btn-btn'} onClick={() => {}}/>
+                    </div>
+                  </InviewMonitor>
+                  <InviewMonitor
+                    classNameNotInView='vis-hidden'
+                    classNameInView='animated zoomIn delay-2s'
+                  >
+                    <div className={'image'}>
+                      <img src={require('../../../shared/img/banner/index_banner.png')}/>
+                    </div>
+                  </InviewMonitor>
                 </div>
-              </div>
-            </Container>
+              </Container>
+    
 
           </div>
           {/* end home__game-detail */}
@@ -174,35 +223,45 @@ class HomePage extends React.Component {
           </div>
           {/* end home__partnership */}
 
-          <Container className="home__features" id={'features'}>
-            <div className="home__features__title-container">
-              <Text className={'home__features-title'} type={Text.types.H2} children={_t('features')} />
-            </div>
+          <InviewMonitor
+            classNameNotInView='vis-hidden'
+            classNameInView='animated zoomIn'
+          >
+            <Container className="home__features" id={'features'}>
+              <div className="home__features__title-container">
+                <Text className={'home__features-title'} type={Text.types.H2} children={_t('features')} />
+              </div>
 
-            <Carousel className="home__features-cards" orientation={Carousel.orientation.VERTICAL} list={_features.map((ft,index) =>
-                              <FeatureCard key={index} className={'home__features-card'} {...ft} />
-                            )} customIndicators={customIndicators}/>
-          </Container>
+              <Carousel className="home__features-cards" orientation={Carousel.orientation.VERTICAL} list={_features.map((ft,index) =>
+                                <FeatureCard key={index} className={'home__features-card'} {...ft} />
+                              )} customIndicators={customIndicators}/>
+            </Container>
+          </InviewMonitor>
           {/* end home__features */}
 
-          <div className="home__subscription">
+          <InviewMonitor
+            classNameNotInView='vis-hidden'
+            classNameInView='animated slideInDown' // fadeInLeft, or fadeInRight
+          >
+            <div className="home__subscription">
 
-            <Container className={'home__subscription-container'}>
-              <div className={'home__subscription-img'}>
-                <img src={require('../../../shared/img/assets/model_example_2.png')}/>
-              </div>
-              <div className={'home__subscription-main'}>
-                <HeaderHighlight>
-                  <b>{_t('get_the_latest_news')}</b>
-                </HeaderHighlight>
-                <p>{_t('get_the_latest_news_desc')}</p>
-                <input type="text" className={'home__subscription-input'} placeholder={_t('your_email_address')} value={this.state.email} onChange={() => {}}/>
-                <ButtonNew className={'home__subscription-btn'} color={ButtonNew.colors.GREEN} label={_t('subscribe')} onClick={() => {
-                  
-                }}/>
-              </div>
-            </Container>
-          </div>
+              <Container className={'home__subscription-container'}>
+                <div className={'home__subscription-img'}>
+                  <img src={require('../../../shared/img/assets/model_example_2.png')}/>
+                </div>
+                <div className={'home__subscription-main'}>
+                  <HeaderHighlight>
+                    <b>{_t('get_the_latest_news')}</b>
+                  </HeaderHighlight>
+                  <p>{_t('get_the_latest_news_desc')}</p>
+                  <input type="text" name="email" className={'home__subscription-input'} placeholder={_t('your_email_address')} onChange={this.validateEmail}/>
+                  <ButtonNew className={'home__subscription-btn'} color={ButtonNew.colors.GREEN} label={_t('subscribe')} onClick={() => {
+                    
+                  }}/>
+                </div>
+              </Container>
+            </div>
+          </InviewMonitor>
           {/* end home__subscription */}
 
         </div>
---
+++ src/views/widgets/InviewMonitor/InviewMonitor.jsx
@@ -0,0 +1,229 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+require("style-loader!./InviewMonitor.scss");
+
+class InviewMonitor extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      newClassName: props.classNameNotInView,
+      childProps: props.childPropsNotInView
+    }
+    this.onIntersectionAll = this.onIntersectionAll.bind(this)
+    this.onIntersection = this.onIntersection.bind(this)
+  }
+
+  componentDidMount() {
+    if (!window.IntersectionObserver) {
+      console.error(`react-inview-monitor found no support for IntersectionObserver.
+Perhaps use a polyfill like: https://cdn.polyfill.io/v2/polyfill.js?features=IntersectionObserver ?`)
+      return
+    }
+    const { useInviewMonitor, intoViewMargin } = this.props;
+    if (
+      !this._element ||
+      !useInviewMonitor ||
+      (typeof useInviewMonitor === 'function' && !useInviewMonitor())
+    ) {
+      return
+    }
+    const options = {
+      rootMargin: intoViewMargin
+    }
+    // any performance benefits from trying to re-use the observer?
+    // possible enhancement to add later on.
+    this.observer = new window.IntersectionObserver(
+      this.onIntersectionAll,
+      options
+    )
+    this.observer.observe(this._element)
+  }
+
+  componentWillUnmount() {
+    this.observer && this.observer.disconnect();
+  }
+
+  onIntersectionAll(entries) {
+    entries.forEach(this.onIntersection);
+  }
+
+  onIntersection(entry) {
+    if (entry.target !== this._element) {
+      // this check only makes sense as long as we _dont_ share the same observer
+      // between component instances.
+      return
+    }
+    const {
+      classNameNotInView,
+      classNameInView,
+      classNameAboveView,
+      classNameNotAboveView,
+      toggleClassNameOnInView,
+      childPropsInView,
+      childPropsNotInView,
+      toggleChildPropsOnInView,
+      onInView,
+      onNotInView,
+      repeatOnInView
+    } = this.props
+
+    const nowInView = entry.isIntersecting;
+    const toggleBehavior =
+      ((classNameInView || classNameAboveView) && toggleClassNameOnInView) ||
+      (childPropsInView && toggleChildPropsOnInView) ||
+      ((onInView || onNotInView) && repeatOnInView);
+
+    if (nowInView && !toggleBehavior) {
+      this.setState({
+        newClassName: classNameInView,
+        childProps: childPropsInView
+      })
+      if (onInView && typeof onInView === 'function') {
+        onInView(entry)
+      }
+      this.observer.unobserve(entry.target)
+      // is there any point trying to determine whether observer is now
+      // no longer observering anything, and hence should be disconnected,
+      // or is this kind of automatic?
+      // To be investigated.
+      return
+    }
+
+    if (toggleBehavior) {
+      // Check if we scrolled past view
+      if (classNameAboveView) {
+        if (
+          // we just left the view
+          !nowInView &&
+          // are we now above it (i.e. scrolled past)
+          entry.boundingClientRect.top <= 0
+        ) {
+          this.setState({
+            newClassName: classNameAboveView
+          })
+        } else {
+          this.setState({
+            newClassName: classNameNotAboveView || ''
+          })
+        }
+        return
+      }
+
+      // check regular in/out of view
+      if (nowInView) {
+        // just entered view
+        this.setState({
+          newClassName: classNameInView,
+          childProps: childPropsInView
+        })
+        if (onInView && typeof onInView === 'function') {
+          onInView(entry)
+        }
+      } else {
+        // just left view
+        this.setState({
+          newClassName: classNameNotInView,
+          childProps: childPropsNotInView
+        })
+        if (onNotInView && typeof onNotInView === 'function') {
+          onNotInView(entry)
+        }
+      }
+    }
+  }
+
+  render() {
+    const { childProps, newClassName } = this.state;
+    let { useInviewMonitor, children } = this.props;
+
+    if (childProps && Object.keys(childProps).length) {
+      children = React.cloneElement(children, childProps)
+    }
+
+    // const array = React.Children.map(children, child => {
+    //   const className = `${children.props.className} ${newClassName}`;
+
+    //   const props = {
+    //     className,
+    //     ref: node => {
+    //       this._element = node;
+    //       const {ref} = child;
+    //       if (typeof ref === 'function') {
+    //         ref(node);
+    //       }
+    //     }
+    //   };
+    //   return React.cloneElement(React.Children.only(children), props);
+    // })
+
+    const className = `${children.props.className} ${newClassName}`;
+    const props = {
+      className,
+      ref: e => {
+        this._element = e;
+      }
+    };
+
+    let newEle;
+    
+    
+    if (typeof children.type === 'function') {
+      newEle = <div
+        className={newClassName}
+        ref={e => {
+          if (e) {
+            this._element = e
+          }
+        }}
+      >
+        {children}
+      </div>;
+    } else {
+      newEle = React.cloneElement(children, props)
+    }
+
+    return (
+      newEle
+    )
+  }
+}
+
+InviewMonitor.propTypes = {
+  // common usage: animate classes in onScrollIntoView, to trigger fade in etc animations
+  classNameInView: PropTypes.string,
+  // can be used to hide elements to be animated in.
+  classNameNotInView: PropTypes.string,
+  // can be used as a trigger for "scrolled past view", f.e. for sticky headers
+  classNameAboveView: PropTypes.string,
+  classNameNotAboveView: PropTypes.string,
+  // can be used to switch classes on/off, for fixed navigation based on scroll point, etc
+  toggleClassNameOnInView: PropTypes.bool,
+
+  // another use for the InviewMonitor is to start passing a prop into an element
+  // only when it has been scrolled into view; f.e. to autoplay a video.
+  childPropsInView: PropTypes.object,
+  childPropsNotInView: PropTypes.object,
+  // can be used to turn prop(s) on/off based of on view, f.e. stop/start video/sound
+  toggleChildPropsOnInView: PropTypes.bool,
+
+  // can be used to track elements coming into view
+  onInView: PropTypes.func,
+  onNotInView: PropTypes.func,
+  repeatOnInView: PropTypes.bool,
+
+  // whether to run any scroll monintoring at all;
+  // because easier to toggle this prop, then toggle not using the component at all.
+  useInviewMonitor: PropTypes.func,
+
+  intoViewMargin: PropTypes.string
+}
+
+InviewMonitor.defaultProps = {
+  classNameNotInView: '',
+  childPropsNotInView: {},
+useInviewMonitor: () => true,
+  intoViewMargin: '-20%'
+}
+
+export default InviewMonitor;
