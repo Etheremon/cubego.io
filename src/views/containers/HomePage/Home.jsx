8cf2d7c63f6201c840dd8cb80242b454afa5b567
Jarvis Nguyen <nvdung149@gmail.com>
2018-10-24 21:35:31 +0700

    [home] minor fix

--- src/views/containers/HomePage/Home.jsx
+++ src/views/containers/HomePage/Home.jsx
@@ -195,7 +195,7 @@ class HomePage extends React.Component {
     
                   <div className={'image'}>
                     <div className={'img'}>
-                      <img className={'img-real'} src={require('../../../shared/img/banner/banner-battle.png')}/>
+                      <img className={'img-real combat'} src={require('../../../shared/img/banner/battle_banner.png')}/>
                     </div>
                     {/*<div className={'img combat'}>*/}
                       {/*<SpriteSheet {...SpriteSource.BATTLE}/>*/}
